6442e84558bfe08fb82e505f4300d12356f05b5e
Mark Grey <mark.grey@nytimes.com>
2013-10-28 21:40:39 -0400

    moar docstrings

--- flask_mab/__init__.py
+++ flask_mab/__init__.py
@@ -1,3 +1,13 @@
+"""
+    flask_mab
+    ~~~~~~~~~
+
+    This module implements all the extension logic for Multi-armed bandit experiments on Flask apps.
+
+    :copyright: (c) 2013 by Mark Grey. 
+    :license: BSD, see LICENSE for more details.
+"""
+
 from flask import current_app,g,request
 import json
 from bandits import *
@@ -9,13 +19,25 @@ except ImportError:
     from flask import _request_ctx_stack as stack
 
 def after_this_request(f):
+    """Uses a list of deferred callbacks to act upon at
+    request end
+    """
     if not hasattr(g, 'after_request_callbacks'):
         g.after_request_callbacks = []
     g.after_request_callbacks.append(f)
     return f
 
 class BanditMiddleware(object):
+    """The main flask extension.
+    Sets up all the necessary tracking for the bandit experiments
+    """
+    
     def __init__(self,app,storage):
+        """Attach MAB logic to a Flask application
+        
+        :param app: A Flask application
+        :param storage: A storage engine instance from the storage module
+        """
         self.app = app
         if app is not None:
             self.init_app(app)
@@ -41,13 +63,32 @@ class BanditMiddleware(object):
         
         #TODO: change this to be config based
         self.debug_headers = True
-        self.init_detection()
+        self._init_detection()
 
     def teardown(self,*args,**kwargs):
+        """Stub for old flask versions
+        """
         pass
 
-    def init_detection(self):
+    def _init_detection(self):
+        """
+        Attaches all request before/after handlers for bandits.
 
+        Nested functions are as follows
+        
+        * pull_decorated_arms: performs arm pulls for flask endpoints
+                               decorated with the :func:`choose_arm`
+                               decorator
+        * detect_last_bandits: Loads any arms already assigned to this user
+                               from the cookie.
+        * persist_bandits:     Saves bandits down to storage engine at the end
+                               of the request
+        * call_after_request_callbacks: Runs all deferred req callbacks
+        * run_reward_decorators: reward any arms for which the decorated request 
+                                 and stored cookie value match
+        * remember_bandit_arms: Sets the cookie for all requests that pulled an arm
+        * send_debug_header: Attaches a header for the MAB to the HTTP response for easier debugging
+        """
         @self.app.before_request
         def pull_decorated_arms():
             for func,bandit in self.pull_endpts:
@@ -95,33 +136,57 @@ class BanditMiddleware(object):
                 return response
 
     def add_bandit(self,name,bandit=None):
+        """Attach a bandit for an experiment
+        
+        :param name: The name of the experiment, will be used for lookups
+        :param bandit: The bandit to use for this experiment
+        :type bandit: Bandit
+        """
         saved_bandits = self.storage.load()
         if name in saved_bandits.keys():
             self.bandits[name] = saved_bandits[name]
         else:
             self.bandits[name] = bandit 
 
-    def pull(self,bandit,arm):
+    def pull(self,bandit_id,arm):
+        """Register a pull (impression) for an arm
+
+        :param bandit: The bandit/experiment name
+        :type bandit: string
+        :param arm: The name of the arm
+        :type arm: string
+        """
         try:
-            self.bandits[bandit].pull_arm(arm)
+            self.bandits[bandit_id].pull_arm(arm)
         except KeyError:
             #bandit does not exist
             pass
 
-    def reward(self,bandit,arm,reward=1):
+    def reward(self,bandit_id,arm,reward=1):
+        """Register an arbitrary "reward" on an arm
+
+        :param bandit_id: The bandit/experiment in question
+        :type bandit_id: string
+        :param arm: The arm to register reward for
+        :type arm: string
+        """
         try:
             self.bandits[bandit].reward_arm(arm,reward)
         except KeyError:
             #bandit does not exist
             pass
 
-    def register_persist_arm(self,bandit_id,arm_id):
-        #persist bandit val in after request callback
+    def _register_persist_arm(self,bandit_id,arm_id):
+        """Puts suggestions on the stack to be saved to a cookie
+        after request
+        """
         if not hasattr(g,'arm_pulls_to_register'):
             g.arm_pulls_to_register = {}
         g.arm_pulls_to_register[bandit_id] = arm_id
 
     def __getitem__(self,key):
+        """Get an bandit/experiment by key
+        """
         return self.bandits[key]
 
     def suggest_arm_for(self,key,also_pull=False):
@@ -130,6 +195,10 @@ class BanditMiddleware(object):
 
         Suggests arms if not in cookie, using cookie val if present
 
+        :param key: The bandit/experiment to get a suggested arm for
+        :type key: string
+        :param also_pull: Should we register a pull/impression at the same time as suggesting
+        :type also_pull: bool
         :raises KeyError: in case requested experiment does not exist
         """
         try:
@@ -141,18 +210,32 @@ class BanditMiddleware(object):
             arm = self.bandits[key].suggest_arm()
             if also_pull:
                 self.pull(key,arm["id"])
-            self.register_persist_arm(key,arm["id"])
+            self._register_persist_arm(key,arm["id"])
             return arm["id"],arm["value"]
         except KeyError,e:
             raise KeyError("No experiment defined for bandit key: %s" % key)
 
     def reward_endpt(self,bandit,reward=1):
+        """Route decorator for rewards.
+
+        :param bandit: The bandit/experiment to register rewards 
+                       for using arm found in cookie.
+        :type bandit: string
+        :param reward: The amount of reward this endpoint should
+                       give its winning arm
+        :type reward: float
+        """
         def decorator(f):
             self.reward_endpts.append((f,bandit,reward)) 
             return f
         return decorator
 
     def choose_arm(self,bandit):
+        """Route decorator for registering an impression conveinently
+
+        :param bandit: The bandit/experiment to register for
+        :type bandit: string
+        """
         def decorator(f):
             self.pull_endpts.append((f,bandit)) 
             return f
